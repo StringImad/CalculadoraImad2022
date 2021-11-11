63a3fd8d39535930d052bfd5fb23058e20d216e2
stringimad <imadelfahssi@gmail.com>
2021-11-11 12:34:43 +0100

    Subida desde clase con la calculadora terminada

--- src/main/java/claculadoraimadclase/CalculadoraImad.java
+++ src/main/java/claculadoraimadclase/CalculadoraImad.java
@@ -12,68 +12,65 @@ import java.util.Scanner;
  * @author imad
  */
 public class CalculadoraImad {
-    
-           private static Scanner sc = new Scanner(System.in);
-    
-           
-           public static void main(String[] args) {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
 
         int operando1 = 1, operando2 = 1;
-        boolean solicitarDatos = true;
 
-        System.out.println("Bienvenido al calculator");
-
-        do {
-
-
-                // Bloque de código candidato a lanzar la excepción
-                operando1 = ComprobadorIntroduccionInt();
-                operando2 = ComprobadorIntroduccionInt();
-                if ((operando1 > -100 && operando1 < 100) && (operando2 > -100 && operando2 < 100)) {
-                    solicitarDatos = false;
-                } else {
-                    System.out.println("Los datos están fuera de rango");
-                }
-
-            
-        } while (solicitarDatos);
-
-        sc.nextLine();
+        System.out.println("Bienvenido a la calculadora");
         String opcion = "";
         int resultado = 0;
         do {
-            mostrarMenu();
-            opcion = sc.nextLine();
+            do {
+                System.out.println("Introduce los numeros para hacer los calculos");
+                // Bloque de código candidato a lanzar la excepción
+                operando1 = ComprobadorIntroduccionInt();
+                operando2 = ComprobadorIntroduccionInt();
+            } while (comprobadorOperador(operando1, operando2));
 
+            mostrarMenu();
+            sc.nextLine();
+            opcion = sc.nextLine();
             switch (opcion) {
                 case "1":
                     resultado = sumar(operando1, operando2);
-
                     break;
                 case "2":
                     resultado = restar(operando1, operando2);
-
                     break;
                 case "3":
                      resultado = dividir(operando1, operando2);
                     break;
-                    
-                     case "4":
-                          resultado = multiplicar(operando1, operando2);
-                break;
-                     case "5":
-                         System.out.println("Ha seleccionado salir");
+                case "4":
+                     resultado = multiplicar(operando1, operando2);
+                    break;
+                case "5":
+                     System.out.println("Ha seleccionado salir");
                     break;
                 default:
                     System.out.println("Opción no válida");
-            }
+                            }
             System.out.println("El resultado es " + resultado);
         } while (!opcion.equals("5"));
 
     }
+    
+    //metodo que recibe dos operandos y  comprueba el rango del operandor y devuelve false si el valor es valido 
+    private static boolean comprobadorOperador(int operador, int operador2) {
+        boolean operadorValido = true;
+        // Bloque de código candidato a lanzar la excepción
+        if ((operador > -100 && operador < 100) && (operador2 > -100 && operador2 < 100)) {
+            operadorValido = false;
+        } else {
+            operadorValido = true;
+        }
+        return operadorValido;
+    }
+
     // Método monstrarMenu. Sólo se utiliza en la clase Calculadora
     // método privado. No recibe parámetros y no devuelve valor (void)
-    // 
     private static void mostrarMenu() {
         System.out.println("Opciones del programa");
         System.out.println("1 - Sumar");
@@ -82,25 +79,30 @@ public class CalculadoraImad {
         System.out.println("2 - Multiplicar");
         System.out.println("5 - Salir");
     }
-    private static int ComprobadorIntroduccionInt(){
-                int numeroIntroducido = 0;
-                boolean repeticion = false;
-  do{   
-      try{   
-         System.out.println("Introduce un numero");
-         numeroIntroducido = sc.nextInt();
-          } catch (InputMismatchException ime) {
-              repeticion = true;
+
+    //metodo que utilizamos para introduir un numer entro y comprueba si es valido, si lo es lo devuelve
+    private static int ComprobadorIntroduccionInt() {
+        int numeroIntroducido = 0;
+        boolean repeticion = false;
+        do {
+            try {
+                System.out.println("Introduce un numero (min -100, max 100)");
+                numeroIntroducido = sc.nextInt();
+            } catch (InputMismatchException ime) {
+                repeticion = true;
                 // Código para tratar el error
                 System.out.println("Se ha introducido texto en lugar de números. "
                         + "Vuelva a introducir los datos");
                 sc.nextLine();
             }
-  }while(repeticion);
+        } while (repeticion);
         return numeroIntroducido;
-        
-    }
 
+    }
+    
+    //Método suma. Sólo se utiliza en la clase Calculadora
+    // método privado. Recibe dos parámetros y devuelve un valor entero
+    // con el resultado de la suma de los parámetros
     private static int sumar(int n1, int n2) {
         int suma = n1 + n2;
         return suma;
@@ -113,15 +115,20 @@ public class CalculadoraImad {
         int restar = n1 - n2;
         return restar;
     }
-    // Método suma. Sólo se utiliza en la clase Calculadora
+
+    // Método dividir. Sólo se utiliza en la clase Calculadora
     // método privado. Recibe dos parámetros y devuelve un valor entero
-    // con el resultado de la suma de los parámetros
- private static int dividir(int n1, int n2) {
-        int dividir = (n1/n2);
+    // con el resultado de la divicion de los parámetros
+    private static int dividir(int n1, int n2) {
+        int dividir = (n1 / n2);
         return dividir;
     }
-  private static int multiplicar(int n1, int n2) {
-        int multiplicar = (n1*n2);
+    
+    // Método multiplicar. Sólo se utiliza en la clase Calculadora
+    // método privado. Recibe dos parámetros y devuelve un valor entero
+    // con el resultado de la multiplicacion de los parámetros
+    private static int multiplicar(int n1, int n2) {
+        int multiplicar = (n1 * n2);
         return multiplicar;
     }
 
